79e51130cae6c653a0252ba4e09b19a7feede3b4
mateogirdillo-gif <mateogirdillo@gmail.com>
2026-09-11 16:42:58 -0500

    excel_service.py

--- excel_service.py
+++ excel_service.py
@@ -298,7 +298,7 @@ def eliminar_pedido(pedido_id):
                 filas_a_borrar=[]
         for row in range(HIST_FIRST_DATA_ROW, ws.max_row + 1):
             pid=ws.cell(row=row, column=HIST_COLS["PEDIDO_ID"]).value
-            if pid and str(pid).strip()==str(pedido_id).strip()
+            if pid and str(pid).strip()==str(pedido_id).strip():
             filas_a_borrar.apppend(row)
         if not filas_a_borrar:
             wb.close()
